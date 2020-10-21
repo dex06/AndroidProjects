3290640e84a438b2bef6137d8a3a996543b2ee28
TRAORE <mohamedtra96@gmail.com>
2020-10-21 14:53:16 +0200

    adding newsletter module

---
+++ newsletter/src/main/java/com/example/newsletter/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.newsletter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.newsletter.fragments.CategoriesFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        changeFragment(CategoriesFragment())
+    }
+
+
+}
+
+fun MainActivity.changeFragment(fragment: Fragment) {
+    supportFragmentManager.beginTransaction().apply {
+        //3) on remplace le contenu du container
+        replace(R.id.fragment_container, fragment)
+        //4) on ajoute la transaction dans la backstack
+        addToBackStack(null)
+    }.commit()
+    // 5) on commit la transaction
+}
---
+++ newsletter/src/main/java/com/example/newsletter/fragments/CategoriesFragment.kt
@@ -0,0 +1,53 @@
+package com.example.newsletter.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsletter.R
+import com.example.newsletter.adapters.CategoryAdapter
+import com.example.newsletter.model.Category
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CategoriesFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CategoriesFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view)
+
+        val articles = listOf<Category>(
+            Category("sante","image1"),
+            Category("sport", "image2"),
+            Category("politique","image3")
+        )
+
+        val adapterRecycler = CategoryAdapter(articles)
+
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        recyclerView.adapter = adapterRecycler
+    }
+
+}
